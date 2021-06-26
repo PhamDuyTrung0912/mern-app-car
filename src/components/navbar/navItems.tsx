a65f4e4ed5b5b5ec5ce33666a966894e38e3ae0b
PhamDuyTrung <trungti09121998@gmail.com>
2021-06-26 17:28:08 +0700

    Menu Navbar

--- src/components/navbar/navItems.tsx
+++ src/components/navbar/navItems.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import styled from "styled-components";
+import styled,{css} from "styled-components";
 import tw from "twin.macro";
+import { slide as Menu } from "react-burger-menu";
+import { useMediaQuery } from "react-responsive";
+import { SCREENS } from "../responsive";
+import menuStyle from "./menuStyle";
 
-const ListContainer=styled.ul`
-    ${tw`
+
+const ListContainer = styled.ul`
+  ${tw`
         flex
         list-none
     `};
 `;
 
-const NavItem=styled.li`
-    ${tw`
+const NavItem = styled.li<{menu?:any}>`
+  ${tw`
         text-xs
         md:text-base
         text-black
@@ -23,10 +28,56 @@ const NavItem=styled.li`
         ease-in-out
         hover:text-gray-700
     `};
+
+    ${({menu})=>menu&& css`
+       ${tw`
+        text-white
+        text-xl
+        mb-3
+        focus:text-white
+       `};
+    `};
+
 `;
 
-export function NavItems(){
-    return(
-        <div>abc</div>
-    )
+export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile) {
+    return (
+      <Menu right styles={menuStyle}>
+        <ListContainer>
+          <NavItem menu>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Cars</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Services</a>
+          </NavItem>
+          <NavItem menu>
+            <a href="#">Contact us</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
+  }
+
+  return (
+    <ListContainer>
+      <NavItem>
+        <a href="#">Home</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Cars</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Services</a>
+      </NavItem>
+      <NavItem>
+        <a href="#">Contact us</a>
+      </NavItem>
+    </ListContainer>
+  );
 }
